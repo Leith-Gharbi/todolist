b8157cbb5dd6c9228bd2ec8e76253426658cbbf2
Gharbi <leithgharbi@yahoo.fr>
2021-02-19 11:47:42 +0100

    .

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const TodoInput = () => {
   return (
     <>
-      <h3>TodoInput</h3>
       <div className="row m-2">
         <input type="text" className="col form-control" />
         <button className="btn btn-primary mx-2">Add</button>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { useSelector } from 'react-redux';
+import { todo } from '../redux/states';
 import TodoItem from './TodoItem';
 
 const TodoList = () => {
+    const todos =useSelector(state => state);
     return ( 
     <div className="my-4">
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
+   {todo.map(todo => {
+       return <TodoItem key={todo.id}/>
+   })}
     </div> );
 }
  
